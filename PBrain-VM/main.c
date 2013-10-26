a08ab6b02d00cf36eaca791ccf045e4de75ee29d
Jake Marsh <jakemmarsh@gmail.com>
2013-10-26 16:39:32 -0400

    implemented linked list of PCBs

--- PBrain-VM/main.c
+++ PBrain-VM/main.c
@@ -67,66 +67,76 @@ int main(int argc, const char * argv[]) {
         sprintf(file_name, "PC.%d.txt", i);
         program_line = i * 100;
         
-        // set base address register and time slice for corresponding process
+        // set initial values for corresponding process control block
         switch(i) {
             case 0: {
                 PCB_0.BAR = i * 100;
                 PCB_0.time_slice = rand() % 10 + 1;
                 PCB_0.idNumber = 0;
+                PCB_0.next = &PCB_1;
                 break;
             }
             case 1: {
                 PCB_1.BAR = i * 100;
                 PCB_1.time_slice = rand() % 10 + 1;
                 PCB_1.idNumber = 1;
+                PCB_1.next = &PCB_2;
                 break;
             }
             case 2: {
                 PCB_2.BAR = i * 100;
                 PCB_2.time_slice = rand() % 10 + 1;
                 PCB_2.idNumber = 2;
+                PCB_2.next = &PCB_3;
                 break;
             }
             case 3: {
                 PCB_3.BAR = i * 100;
                 PCB_3.time_slice = rand() % 10 + 1;
                 PCB_3.idNumber = 3;
+                PCB_3.next = &PCB_4;
                 break;
             }
             case 4: {
                 PCB_4.BAR = i * 100;
                 PCB_4.time_slice = rand() % 10 + 1;
                 PCB_4.idNumber = 4;
+                PCB_4.next = &PCB_5;
                 break;
             }
             case 5: {
                 PCB_5.BAR = i * 100;
                 PCB_5.time_slice = rand() % 10 + 1;
                 PCB_5.idNumber = 5;
+                PCB_5.next = &PCB_6;
                 break;
             }
             case 6: {
                 PCB_6.BAR = i * 100;
                 PCB_6.time_slice = rand() % 10 + 1;
                 PCB_6.idNumber = 6;
+                PCB_6.next = &PCB_7;
                 break;
             }
             case 7: {
                 PCB_7.BAR = i * 100;
                 PCB_7.time_slice = rand() % 10 + 1;
                 PCB_7.idNumber = 7;
+                PCB_7.next = &PCB_8;
                 break;
             }
             case 8: {
                 PCB_8.BAR = i * 100;
                 PCB_8.time_slice = rand() % 10 + 1;
                 PCB_8.idNumber = 8;
+                PCB_8.next = &PCB_9;
                 break;
             }
             case 9: {
                 PCB_9.BAR = i * 100;
                 PCB_9.time_slice = rand() % 10 + 1;
                 PCB_9.idNumber = 9;
+                PCB_9.next = NULL;
                 break;
             }
             default: {
